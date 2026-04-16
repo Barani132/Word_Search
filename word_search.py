0606a035d2a8f1b477b584f76308b4d547c55941
Barani132 <barani.b2137@gmail.com>
2026-04-16 21:32:45 +0530

    Implement word search using backtracking

---
+++ word_search.py
@@ -0,0 +1,25 @@
+#Word Search (Backtracking Grid)
+board = [
+ ['A','B','C','E'],
+ ['S','F','C','S'],
+ ['A','D','E','E']
+]
+
+word = "ABCCED"
+
+def dfs(i, j, k):
+    if k == len(word):
+        return True
+    if i<0 or j<0 or i>=len(board) or j>=len(board[0]) or board[i][j] != word[k]:
+        return False
+
+    temp = board[i][j]
+    board[i][j] = "#"
+
+    found = (dfs(i+1,j,k+1) or dfs(i-1,j,k+1) or
+             dfs(i,j+1,k+1) or dfs(i,j-1,k+1))
+
+    board[i][j] = temp
+    return found
+
+print(dfs(0,0,0))
